2f0c84aeadb094349cf16dc69411af3e0d503ba0
LING <1158149596@qq.com>
2022-01-26 10:10:49 +0800

    add producer

---
+++ src/main/java/com/example/kafkastudy/constant/KafkaProducerProperties.java
@@ -0,0 +1,11 @@
+package com.example.kafkastudy.constant;
+
+/**
+ * @author LING lbh19970425@gmail.com
+ * @date 2022/1/26 09:48
+ */
+public class KafkaProducerProperties {
+    public static final String KAFKA_BROKER_URL = "localhost:9092";
+    public static final String KAFKA_DEMO_TOPIC = "topic_demo";
+    public static final String KAFKA_CLIENT_ID = "producer.client.id.demo";
+}
---
+++ src/main/java/com/example/kafkastudy/kafka/Producer.java
@@ -0,0 +1,48 @@
+package com.example.kafkastudy.kafka;
+
+import com.example.kafkastudy.constant.KafkaProducerProperties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+/**
+ * @author LING lbh19970425@gmail.com
+ * @date 2022/1/26 09:47
+ */
+@Component
+public class Producer {
+    private KafkaProducer<String,String> producer;
+
+    @PostConstruct
+    public void init(){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaProducerProperties.KAFKA_BROKER_URL);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, KafkaProducerProperties.KAFKA_CLIENT_ID);
+        //同步发送时，可选配置重试次数，重试指定次数后还失败才会抛出异常
+        //props.put(ProducerConfig.RETRIES_CONFIG,3);
+        producer = new KafkaProducer<>(props);
+    }
+
+    public void send(ProducerRecord<String,String> record){
+        try{
+            //异步发送，发后即忘
+            producer.send(record);
+            /*
+             *同步发送，可靠性更高
+             * Future<RecordMetadata> future = producer.send(record);
+             * RecordMetadata recordMetadata = future.get();
+             * */
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
